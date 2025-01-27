7b5f1b14bedb337905271b08a50f3d23c796484f
George Mushore <gmu003@gmail.com>
2025-01-27 11:03:51 +0100

    listing all links

--- src/main_test.go
+++ src/main_test.go
@@ -1,33 +1,159 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestParser(t *testing.T) {
 
 	tests := []struct {
-		route   string
+		name    string
 		args    string
 		want    string
 		wantErr bool
-	}{{
-		route:   "/home",
-		args:    `<html><body><h1>Hello, Go!</h1><a href="/home">Home</a></body></html>`,
-		want:    "Home",
-		wantErr: false,
-	},
+	}{
+		{
+			name:    "Retrieve link",
+			args:    `<html><body><h1>Hello, Go!</h1><a href="/home">Home</a></body></html>`,
+			want:    "/home",
+			wantErr: false,
+		},
+		{name: "Return the valid link",
+			args: `<a href="#">This link points to nothing</a>
+						<a href="/about">This link points to an internal page</a>
+						<a href="https://youtube.com">This link points to an external page</a>
+						`,
+			want:    "/about",
+			wantErr: false,
+		},
+		{name: "Return the valid link",
+			args: `<a>This link points to nothing</a>
+						<a href="/about">This link points to an internal page</a>
+						<a href="https://youtube.com">This link points to an external page</a>
+						`,
+			want:    "/about",
+			wantErr: false,
+		},
+		{name: "Return the valid link when class",
+			args: `<a>This link points to nothing</a>
+						<a href="/about">This link points to an internal page</a>
+						<a class="text-teal-400 hover:text-teal-200" href="https://youtube.com">This link points to an external page</a>
+						`,
+			want:    "/about",
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.route, func(t *testing.T) {
-			got, err := HtmlParser(tt.args)
+		t.Run(tt.name, func(t *testing.T) {
+			rv := strings.NewReader(tt.args)
+			got, err := HtmlParser(rv)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("HtmlParser() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+			for k, _ := range got {
+
+				if k != tt.want {
+					t.Errorf("HtmlParser() = %v, want %v", got, tt.want)
+				}
+			}
+		})
+	}
+}
+
+func TestValidUrl(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		args    string
+		want    bool
+		wantErr bool
+	}{
+		{
+			name:    "Check invalid",
+			args:    "#",
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name:    "Check valid",
+			args:    "/about",
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "Check query params",
+			args:    "/anime?name=mha",
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "Check external",
+			args:    "https://youtube.com",
+			want:    false,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := IsValidUrl(tt.args)
+
 			if got != tt.want {
 				t.Errorf("HtmlParser() = %v, want %v", got, tt.want)
 			}
 		})
 	}
+}
+
+func TestSetCount(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		args    Set
+		want    int
+		wantErr bool
+	}{
+		{
+			name:    "items count",
+			args:    Set{"/a": {}, "/b": {}},
+			want:    2,
+			wantErr: false,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.args.Count()
+
+			if got != tt.want {
+				t.Errorf("HtmlParser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+	tests_2 := []struct {
+		name    string
+		args    [2]string
+		want    int
+		wantErr bool
+	}{
+		{
+			name:    "items count",
+			args:    [...]string{"/a", "b"},
+			want:    2,
+			wantErr: false,
+		}}
+
+	for _, tt := range tests_2 {
+		new_set := NewSet()
+		for _, k := range tt.args {
+			new_set.Add(k)
+		}
+
+		t.Run(tt.name, func(t *testing.T) {
+			got := new_set.Count()
+			if got != tt.want {
+				t.Errorf("HtmlParser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
 }
